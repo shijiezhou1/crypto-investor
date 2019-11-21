40e1660c8fad0842142da7e743f5f1ed6bf873c6
Shijie Zhou <cs5129606@gmail.com>
2019-11-21 00:00:18 -0700

    adding readme and index

---
+++ README.md
@@ -0,0 +1,33 @@
+# Crypto-Investor
+
+The command line program show how to get all the crypto currencies and convert to different currencies.
+
+## Project setup
+
+```
+npm install
+```
+
+### Given no parameters, return the latest portfolio value per token in USD
+
+```
+node index.js
+```
+
+### Given a token, return the latest portfolio value for that token in USD
+
+```
+node index.js BTC
+```
+
+### Given a date, return the portfolio value per token in USD on that date
+
+```
+node index.js 2018-01-01
+```
+
+### Given a date and a token, return the portfolio value of that token in USD on that date
+
+```
+node index.js BTC
+```
--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const argv = require('optimist').argv;
 const cc = require('cryptocompare');
 cc.setApiKey('2605c530c671f9c3ee1b94ca290d354b85d998dcdf43fc1e8349a44aa9b7f6f1')
 const argument = argv._;
+console.log('ARGUMENTS', argument);
 
 let SYMBOLS = ['BTC', 'ETH', 'XRP'];
 let CURRENCIES = ['USD'];
-console.log('current argument length', argument.length);
 
 function isToken(arg) { // CHECK IF TOKE IS VALID e.g. BTC etc
     return SYMBOLS.some(r => arg.indexOf(r) >= 0) ? true : false
@@ -23,17 +23,18 @@ if (argument.length < 1) { // FIND NO ARGUMENTS
         .catch(console.error)
 
 } else if (argument.length == 2) { // FIND TWO ARGUMENTS
-    console.log('find two argument ', argument[0], argument[1]);
-
     // IGNORE ORDER 
     if (isDate(argument[0]) && isToken(argument[1])) {
-
+        cc.priceHistorical(argument[1], CURRENCIES[0], new Date(argument[0])).then(prices => {
+            console.log(prices);
+        }).catch(console.error)
     } else if (isDate(argument[1]) && isToken(argument[0])) {
-
+        cc.priceHistorical(argument[0], CURRENCIES[0], new Date(argument[1])).then(prices => {
+            console.log(prices);
+        }).catch(console.error)
     } else {
         console.log('invalid toke or date enter: e.g. BTC 2000-01-01 or 2000-01-01 BTC');
     }
-
 
 } else if (argument.length == 1) { // FIND ONE ARGUMENT
 
